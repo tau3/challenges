e962e01fcf0a10a34e8e2079ac1211b3dcaf425b
Sergey <smr.oznob@gmail.com>
2019-02-15 15:23:33 +0400

    refactored PalindromeIndex

--- hackerrank/haskell/PalindromeIndex.hs
+++ hackerrank/haskell/PalindromeIndex.hs
@@ -1,21 +1,27 @@
-solve :: String -> Int
-solve s =
-  if isPalindrome s
-    then -1
-    else firstMatch' s 0
+import qualified Data.Vector as V
+
+solve :: V.Vector Char -> Int
+solve original = solve' original 0
   where
-    firstMatch' xs i
-      | i == length xs = -1
+    solve' v i
+      | V.length v < 2 = -1
+      | V.last v == V.head v =
+        let v' = V.init $ V.tail v
+         in solve' v' (i + 1)
       | otherwise =
-        if isPalindrome (xs `chop` i)
-          then i
-          else firstMatch' xs (i + 1)
+        if isPalindrome $ V.init v
+          then V.length original - 1 - i
+          else if isPalindrome $ V.tail v
+                 then i
+                 else -1
 
-isPalindrome :: String -> Bool
-isPalindrome s = s == reverse s
-
-chop :: String -> Int -> String
-chop xs i = take i xs ++ drop (i + 1) xs
+isPalindrome :: V.Vector Char -> Bool
+isPalindrome v
+  | V.length v < 2 = True
+  | V.last v == V.head v =
+    let v' = V.init $ V.tail v
+     in isPalindrome v'
+  | otherwise = False
 
 main :: IO ()
-main = interact $ unlines . map (show . solve) . tail . lines
+main = interact $ unlines . map (show . solve . V.fromList) . tail . lines
