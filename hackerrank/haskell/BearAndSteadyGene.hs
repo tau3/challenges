78a1ff8aad42d84232b844d8376dfe261fdfee40
Sergey <smr.oznob@gmail.com>
2019-02-15 14:39:52 +0400

    refactored BearAndSteadyGene

--- hackerrank/haskell/BearAndSteadyGene.hs
+++ hackerrank/haskell/BearAndSteadyGene.hs
@@ -1,33 +1,34 @@
-import qualified Data.HashMap.Strict as Map
-import Data.List
-import Data.Maybe
+import qualified Data.HashMap.Strict as M (HashMap, alter, empty, foldr, update)
+import Data.Maybe (isNothing)
+import qualified Data.Vector as V (Vector, (!), foldr, fromList)
 
-countChars :: String -> Map.HashMap Char Int
-countChars = foldl' (flip (Map.alter f)) Map.empty
+countChars :: V.Vector Char -> M.HashMap Char Int
+countChars = V.foldr (M.alter f) M.empty
   where
     f maybeVal
       | isNothing maybeVal = Just 1
       | otherwise = fmap (+ 1) maybeVal
 
-isBalanced :: Map.HashMap Char Int -> Int -> Bool
-isBalanced m n = Map.foldl' (\acc v -> acc && (v <= n)) True m
+isBalanced :: M.HashMap Char Int -> Int -> Bool
+isBalanced m n = M.foldr (\v acc -> acc && (v <= n)) True m
 
 solve :: String -> Int
-solve s = solve' (countChars s) 0 0 l
+solve gene = solve' (countChars s) 0 0 l
   where
+    s = V.fromList gene
     l = length s
     n = l `div` 4
     solve' m i j res
       | (i >= l) || (j >= l) = res
       | not (isBalanced m n) =
-        let m' = Map.update (\v -> return (v - 1)) (s !! j) m
+        let m' = M.update (\v -> return (v - 1)) (s V.! j) m
             j' = j + 1
          in solve' m' i j' res
       | otherwise =
         let res' = min res (j - i)
             i' = i + 1
-            m' = Map.update (return . (+ 1)) (s !! i) m
+            m' = M.update (return . (+ 1)) (s V.! i) m
          in solve' m' i' j res'
 
 main :: IO ()
-main = getLine >> getLine >>= return . solve >>= print
+main = (solve <$> (getLine >> getLine)) >>= print
