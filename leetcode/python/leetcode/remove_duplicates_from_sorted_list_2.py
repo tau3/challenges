d8b0cc3524e8e255af0a1e8ca2265dd8db350950
Sergey <smr.oznob@gmail.com>
2019-04-18 17:40:43 +0400

    working recursive solution

--- leetcode/python/leetcode/remove_duplicates_from_sorted_list_2.py
+++ leetcode/python/leetcode/remove_duplicates_from_sorted_list_2.py
@@ -6,19 +6,15 @@ class ListNode:
 
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
-        if not head:
-            return None
-        if head.next is None:
+        if head is None or head.next is None:
             return head
 
-        current = head.next
-        prev = head
-        val = current.val
-        while current:
-            if val == current.val:
-                prev.next = current.next
-            else:
-                val = current.val
-            prev = current
-            current = current.next
-        return head
+        if head.val == head.next.val:
+            current = head.next.next
+            while current is not None and current.val == head.val:
+                current = current.next
+            result = self.deleteDuplicates(current)
+        else:
+            head.next = self.deleteDuplicates(head.next)
+            result = head
+        return result
