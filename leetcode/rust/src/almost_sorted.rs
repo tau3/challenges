081abbec8a3705504b20bc6c8b1e0bbc48263ad1
Sergey Rozhnov <smr.oznob@gmail.com>
2020-03-14 02:58:16 +0400

    almost solved max_min

---
+++ leetcode/rust/src/almost_sorted.rs
@@ -0,0 +1,73 @@
+use std::io;
+
+// doesn't work
+fn almost_sorted() {
+    let arr = read_input_arr();
+    let sorted = sorted_copy(&arr);
+
+    if sorted == arr {
+        println!("yes");
+        return;
+    }
+
+    let unsorted_indexes = get_unsorted_indexes(&arr, &sorted);
+
+    if unsorted_indexes.len() == 2 {
+        println!("yes");
+        println!("{} {}", unsorted_indexes[0] + 1, unsorted_indexes[1] + 1);
+        return;
+    } else {
+        let l = unsorted_indexes[0];
+        let r = unsorted_indexes.last().unwrap().to_owned();
+
+        let segment = slice_vector(&arr, l, r);
+
+        let reverse_sorted = reverse_sort(&segment);
+        if segment == reverse_sorted {
+            println!("yes");
+            println!("reverse {} {}", l, r);
+        } else {
+            println!("no")
+        }
+    }
+}
+
+fn slice_vector(v: &Vec<u16>, l: usize, r: usize) -> Vec<u16> {
+    let mut result = Vec::new();
+    result.copy_from_slice(&v[l..r]);
+    return result;
+}
+
+fn get_unsorted_indexes(arr: &Vec<u16>, sorted: &Vec<u16>) -> Vec<usize> {
+    let mut unsorted_indexes: Vec<usize> = Vec::new();
+    for (i, val) in arr.iter().enumerate() {
+        if *val != sorted[i] {
+            unsorted_indexes.push(i);
+        }
+    }
+    return unsorted_indexes;
+}
+
+fn reverse_sort(v: &Vec<u16>) -> Vec<u16> {
+    let mut result = sorted_copy(v);
+    result.reverse();
+    return result;
+}
+
+fn sorted_copy(v: &Vec<u16>) -> Vec<u16> {
+    let mut result = v.clone();
+    result.sort();
+    return result;
+}
+
+fn read_input_arr() -> Vec<u16> {
+    let mut line = String::new();
+    let std_in = io::stdin();
+    std_in.read_line(&mut line).unwrap();
+    std_in.read_line(&mut line).unwrap();
+
+    let arr: Vec<u16> = line.split_whitespace()
+        .map(|token| { token.parse().unwrap() })
+        .collect();
+    return arr;
+}
--- leetcode/rust/src/main.rs
+++ leetcode/rust/src/main.rs
@@ -1,72 +1,33 @@
+use std::convert::TryFrom;
 use std::io;
 
 fn main() {
-    let arr = read_input_arr();
-    let sorted = sorted_copy(&arr);
+    let n = read_u32();
+    let k = usize::try_from(read_u32()).unwrap();
 
-    if sorted == arr {
-        println!("yes");
-        return;
+    let mut arr: Vec<u32> = Vec::new();
+    for _i in 0..n {
+        arr.push(read_u32());
     }
 
-    let unsorted_indexes = get_unsorted_indexes(&arr, &sorted);
-
-    if unsorted_indexes.len() == 2 {
-        println!("yes");
-        println!("{} {}", unsorted_indexes[0] + 1, unsorted_indexes[1] + 1);
-        return;
-    } else {
-        let l = unsorted_indexes[0];
-        let r = unsorted_indexes.last().unwrap().to_owned();
-
-        let segment = slice_vector(&arr, l, r);
-
-        let reverse_sorted = reverse_sort(&segment);
-        if segment == reverse_sorted {
-            println!("yes");
-            println!("reverse {} {}", l, r);
-        } else {
-            println!("no")
-        }
-    }
+    println!("{}", max_min(k, &arr));
 }
 
-fn slice_vector(v: &Vec<u16>, l: usize, r: usize) -> Vec<u16> {
-    let mut result = Vec::new();
-    result.copy_from_slice(&v[l..r]);
-    return result;
-}
-
-fn get_unsorted_indexes(arr: &Vec<u16>, sorted: &Vec<u16>) -> Vec<usize> {
-    let mut unsorted_indexes: Vec<usize> = Vec::new();
-    for (i, val) in arr.iter().enumerate() {
-        if *val != sorted[i] {
-            unsorted_indexes.push(i);
-        }
-    }
-    return unsorted_indexes;
-}
-
-fn reverse_sort(v: &Vec<u16>) -> Vec<u16> {
-    let mut result = sorted_copy(v);
-    result.reverse();
-    return result;
-}
-
-fn sorted_copy(v: &Vec<u16>) -> Vec<u16> {
-    let mut result = v.clone();
-    result.sort();
-    return result;
-}
-
-fn read_input_arr() -> Vec<u16> {
+fn read_u32() -> u32 {
     let mut line = String::new();
-    let std_in = io::stdin();
-    std_in.read_line(&mut line).unwrap();
-    std_in.read_line(&mut line).unwrap();
+    io::stdin().read_line(&mut line).unwrap();
+    let result = line.trim().parse().unwrap();
+    return result;
+}
 
-    let arr: Vec<u16> = line.split_whitespace()
-        .map(|token| { token.parse().unwrap() })
-        .collect();
-    return arr;
+fn max_min(k: usize, arr: &Vec<u32>) -> u32 {
+    let mut sorted = arr.clone();
+    sorted.sort();
+
+    let mut diffs = Vec::new();
+    for i in 0..sorted.len() - k {
+        diffs.push(sorted[i + k - 1] - sorted[i]);
+    }
+    let result = diffs.iter().min();
+    return result.unwrap().to_owned();
 }
