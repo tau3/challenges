855d0c06fd3db4df636b5072a9e007c8c3682df5
Sergey Rozhnov <smr.oznob@gmail.com>
2020-10-06 15:40:30 +0400

    p496 - working

--- leetcode/cpp/next-greater-element-i.cpp
+++ leetcode/cpp/next-greater-element-i.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include "gtest/gtest.h"
+#include <algorithm>
 
 using namespace std;
 
@@ -11,18 +12,23 @@ public:
         vector<int> result(nums1.size(), -1);
         for (int i = 0; i < static_cast<int>(nums1.size()); i++)
         {
-            int lvalue = nums1[i];
-            int next = lvalue;
-            for (int rvalue : nums2)
+            for (int j = index_of(nums2, nums1[i]) + 1; j < static_cast<int>(nums2.size()); j++)
             {
-                if (rvalue > lvalue)
+                if (nums2[j] > nums1[i])
                 {
-                    next = (next > lvalue) ? min(next, rvalue) : rvalue;
+                    result[i] = nums2[j];
+                    break;
                 }
             }
-            result[i] = (nums1[i] != lvalue) ? lvalue : -1;
         }
         return result;
+    }
+
+private:
+    int index_of(const vector<int> &nums, int element)
+    {
+        auto iterator = find(nums.begin(), nums.end(), element);
+        return iterator - nums.begin();
     }
 };
 
@@ -31,6 +37,15 @@ TEST(P496, test_case_1)
     vector<int> nums1{4, 1, 2};
     vector<int> nums2{1, 3, 4, 2};
     vector<int> expected{-1, 3, -1};
+    Solution solution;
+    ASSERT_EQ(expected, solution.nextGreaterElement(nums1, nums2));
+}
+
+TEST(P496, test_case_2)
+{
+    vector<int> nums1{2, 4};
+    vector<int> nums2{1, 2, 3, 4};
+    vector<int> expected{3, -1};
     Solution solution;
     ASSERT_EQ(expected, solution.nextGreaterElement(nums1, nums2));
 }
