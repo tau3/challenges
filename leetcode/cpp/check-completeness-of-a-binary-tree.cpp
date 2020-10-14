50f33c835edefd5eb4e24f4b3063a7936ce79154
Sergey Rozhnov <smr.oznob@gmail.com>
2020-10-14 17:01:23 +0400

    refactored solution

--- leetcode/cpp/check-completeness-of-a-binary-tree.cpp
+++ leetcode/cpp/check-completeness-of-a-binary-tree.cpp
@@ -1,49 +1,39 @@
-#include <vector>
-#include <deque>
-#include <iostream>
+#include <queue>
 
 using namespace std;
 
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
-class Solution
-{
+class Solution {
 public:
-    bool isCompleteTree(TreeNode *root)
-    {
-        bool is_gap = false;
-        deque<TreeNode *> dq{root};
-        while (!dq.empty())
-        {
-            TreeNode *node = dq.front();
-            dq.pop_front();
+  bool isCompleteTree(TreeNode *root) {
+    bool is_gap = false;
+    queue<TreeNode *> dq;
+    dq.push(root);
+    while (!dq.empty()) {
+      TreeNode *node = dq.front();
+      dq.pop();
 
-            if (node == nullptr)
-            {
-                is_gap = true;
-                continue;
-            }
-            else
-            {
-                if (is_gap)
-                {
-                    return false;
-                }
-                else
-                {
-                    dq.push_back(node->left);
-                    dq.push_back(node->right);
-                }
-            }
+      if (node == nullptr) {
+        is_gap = true;
+        continue;
+      } else {
+        if (is_gap) {
+          return false;
+        } else {
+          dq.push(node->left);
+          dq.push(node->right);
         }
-        return true;
+      }
     }
+    return true;
+  }
 };
