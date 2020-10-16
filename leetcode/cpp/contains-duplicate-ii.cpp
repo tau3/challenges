08b2c0f256d136054125e38df45ee1e013032e8e
Sergey Rozhnov <smr.oznob@gmail.com>
2020-10-16 14:40:56 +0400

    refactored solution

--- leetcode/cpp/contains-duplicate-ii.cpp
+++ leetcode/cpp/contains-duplicate-ii.cpp
@@ -1,43 +1,41 @@
 #include "gtest/gtest.h"
-#include <map>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 class Solution {
-public:
-  bool containsNearbyDuplicate(vector<int> &nums, int k) {
-    map<int, int> cache;
-    for (int i = 0; i < static_cast<int>(nums.size()); ++i) {
-      int num = nums[i];
-      auto pos = cache.find(num);
-      if (cache.find(num) == cache.end()) {
-        cache[num] = i;
-      } else {
-        if ((i - pos->second) <= k) {
-          return true;
+  public:
+    bool containsNearbyDuplicate(vector<int> &nums, int k) {
+        unordered_map<int, int> cache;
+        for (int i = 0; i < static_cast<int>(nums.size()); ++i) {
+            int num = nums[i];
+            auto pos = cache.find(num);
+            if (cache.find(num) != cache.end()) {
+                if ((i - pos->second) <= k) {
+                    return true;
+                }
+            }
+            cache[num] = i;
         }
-        cache[num] = i;
-      }
+        return false;
     }
-    return false;
-  }
 };
 
 TEST(P213, test_case_1) {
-  Solution solution;
-  vector<int> nums = {1, 2, 3, 1};
-  ASSERT_TRUE(solution.containsNearbyDuplicate(nums, 3));
+    Solution solution;
+    vector<int> nums = {1, 2, 3, 1};
+    ASSERT_TRUE(solution.containsNearbyDuplicate(nums, 3));
 }
 
 TEST(P213, test_case_2) {
-  Solution solution;
-  vector<int> nums = {1, 0, 1, 1};
-  ASSERT_TRUE(solution.containsNearbyDuplicate(nums, 1));
+    Solution solution;
+    vector<int> nums = {1, 0, 1, 1};
+    ASSERT_TRUE(solution.containsNearbyDuplicate(nums, 1));
 }
 
 TEST(P213, test_case_3) {
-  Solution solution;
-  vector<int> nums = {1, 2, 3, 1, 2, 3};
-  ASSERT_FALSE(solution.containsNearbyDuplicate(nums, 2));
+    Solution solution;
+    vector<int> nums = {1, 2, 3, 1, 2, 3};
+    ASSERT_FALSE(solution.containsNearbyDuplicate(nums, 2));
 }
