949d52bb44cdaa1d5ccade39087e73ec98005865
tau3 <smr.oznob@gmail.com>
2021-07-09 17:38:36 +0400

    refactored task1

--- sprout/src/main/java/com/sprout/test/Task1.java
+++ sprout/src/main/java/com/sprout/test/Task1.java
@@ -11,6 +11,22 @@ import java.util.stream.Collectors;
 
 public class Task1 {
     public int solution(int N, int[] A, int[] B) {
+        List<Integer> sortedVertexes = sortVertexesByFrequency(A, B);
+        Map<Integer, Integer> vertexToWeight = assignWeights(N, sortedVertexes);
+        return calculateSum(A, B, vertexToWeight);
+    }
+
+    private int calculateSum(int[] A, int[] B, Map<Integer, Integer> vertexToValue) {
+        int result = 0;
+        for (int i = 0; i < A.length; i++) {
+            int start = A[i];
+            int end = B[i];
+            result += vertexToValue.get(start) + vertexToValue.get(end);
+        }
+        return result;
+    }
+
+    private List<Integer> sortVertexesByFrequency(int[] A, int[] B) {
         List<Integer> vertexAppearances = new ArrayList<>();
 
         vertexAppearances.addAll(arrayToList(A));
@@ -19,29 +35,21 @@ public class Task1 {
 
         Map<Integer, Long> vertexToAppearances = vertexAppearances.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        System.out.println("vertex to app " + vertexToAppearances);
 
-        List<Integer> sortedVertexes = vertexAppearances.stream()
+        return vertexAppearances.stream()
                 .distinct()
                 .sorted(Comparator.comparing(vertexToAppearances::get).reversed())
                 .toList();
-        System.out.println("sorted " + sortedVertexes);
+    }
 
+    private Map<Integer, Integer> assignWeights(int N, List<Integer> sortedVertexes) {
         Map<Integer, Integer> vertexToValue = new HashMap<>();
         int currentWeight = N;
         for (int vertex : sortedVertexes) {
             vertexToValue.put(vertex, currentWeight);
             currentWeight--;
         }
-
-        int result = 0;
-        for (int i = 0; i < A.length; i++) {
-            int start = A[i];
-            int end = B[i];
-            result += vertexToValue.get(start) + vertexToValue.get(end);
-        }
-
-        return result;
+        return vertexToValue;
     }
 
     private static List<Integer> arrayToList(int[] array) {
