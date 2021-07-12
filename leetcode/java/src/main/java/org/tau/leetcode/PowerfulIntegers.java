161a9e1f35be52350b1ce88531b06a6d51f4b446
tau3 <smr.oznob@gmail.com>
2021-07-12 17:34:54 +0400

    refactored PowerfulIntegers

--- leetcode/java/src/main/java/org/tau/leetcode/PowerfulIntegers.java
+++ leetcode/java/src/main/java/org/tau/leetcode/PowerfulIntegers.java
@@ -4,39 +4,25 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class PowerfulIntegers {
-    private static final int MAX_POW = 20;
-
     public List<Integer> powerfulIntegers(int x, int y, int bound) {
-        int[] xPows = new int[MAX_POW];
-        xPows[0] = 1;
-        int[] yPows = new int[MAX_POW];
-        yPows[0] = 1;
-
         Collection<Integer> result = new HashSet<>();
-        outerloop:
-        for (int i = 0; i < MAX_POW; i++) {
-            for (int j = 0; j < MAX_POW; j++) {
-                if (xPows[i] == 0) {
-                    xPows[i] = (int) StrictMath.pow(x, i);
-                }
-                if (xPows[i] >= bound) {
-                    break outerloop;
-                }
-
-                if (yPows[j] == 0) {
-                    yPows[j] = (int) StrictMath.pow(y, j);
-                }
-
-                int candidate = xPows[i] + yPows[j];
+        int i = 1, j = 1;
+        do {
+            do {
+                int candidate = i + j;
                 if (candidate <= bound) {
                     result.add(candidate);
-                } else {
-                    break;
                 }
+                j *= y;
             }
+            while ((y != 1) && (j < bound));
+            i *= x;
+            j = 1;
         }
+        while ((x != 1) && (i < bound));
 
         return new ArrayList<>(result);
     }
