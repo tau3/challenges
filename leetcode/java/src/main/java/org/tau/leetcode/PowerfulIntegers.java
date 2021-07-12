305e193080854eb25eb012798e81086670d70280
tau3 <smr.oznob@gmail.com>
2021-07-12 17:10:45 +0400

    solved PowerfulIntegers

--- leetcode/java/src/main/java/org/tau/leetcode/PowerfulIntegers.java
+++ leetcode/java/src/main/java/org/tau/leetcode/PowerfulIntegers.java
@@ -2,33 +2,42 @@ package org.tau.leetcode;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
-import java.util.NavigableSet;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 class PowerfulIntegers {
+    private static final int MAX_POW = 20;
+
     public List<Integer> powerfulIntegers(int x, int y, int bound) {
-        Collection<Integer> xPows = new TreeSet<>();
-        for (int i = 0, xPow = 1; xPow <= bound; i++, xPow = (int) StrictMath.pow(x, i)) {
-            xPows.add(xPow);
+        int[] xPows = new int[MAX_POW];
+        xPows[0] = 1;
+        int[] yPows = new int[MAX_POW];
+        yPows[0] = 1;
+
+        Collection<Integer> result = new HashSet<>();
+        outerloop:
+        for (int i = 0; i < MAX_POW; i++) {
+            for (int j = 0; j < MAX_POW; j++) {
+                if (xPows[i] == 0) {
+                    xPows[i] = (int) StrictMath.pow(x, i);
+                }
+                if (xPows[i] >= bound) {
+                    break outerloop;
+                }
+
+                if (yPows[j] == 0) {
+                    yPows[j] = (int) StrictMath.pow(y, j);
+                }
+
+                int candidate = xPows[i] + yPows[j];
+                if (candidate <= bound) {
+                    result.add(candidate);
+                } else {
+                    break;
+                }
+            }
         }
 
-        NavigableSet<Integer> yPows = new TreeSet<>();
-        for (int i = 0, yPow = 1; yPow <= bound; i++, yPow = (int) StrictMath.pow(y, i)) {
-            yPows.add(yPow);
-
-        }
-
-        List<Integer> result = new ArrayList<>();
-        for (int xPow : xPows) {
-            yPows.headSet(bound - xPow, true)
-                    .stream()
-                    .map(yPow -> yPow + xPow)
-                    .forEach(result::add);
-        }
-        return result.stream()
-                .distinct()
-                .collect(Collectors.toList());
+        return new ArrayList<>(result);
     }
 }
