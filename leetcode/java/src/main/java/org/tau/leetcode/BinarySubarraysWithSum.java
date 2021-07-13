be8555b0f525ba7b1080277d6ec101b650046325
tau3 <smr.oznob@gmail.com>
2021-07-13 18:38:38 +0400

    solved p930 with prefix sums

--- leetcode/java/src/main/java/org/tau/leetcode/BinarySubarraysWithSum.java
+++ leetcode/java/src/main/java/org/tau/leetcode/BinarySubarraysWithSum.java
@@ -1,7 +1,11 @@
 package org.tau.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class BinarySubarraysSum {
-    public int numSubarraysWithSum(int[] nums, int goal) {
+    // O(n^2) space -> too bad
+    public int numSubarraysWithSumDp(int[] nums, int goal) {
         int[][] dp = new int[nums.length][nums.length];
         int result = 0;
         for (int start = 0; start < nums.length; start++) {
@@ -15,6 +19,21 @@ class BinarySubarraysSum {
                     result++;
                 }
             }
+        }
+        return result;
+    }
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int[] prefixes = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            prefixes[i + 1] = prefixes[i] + nums[i];
+        }
+
+        int result = 0;
+        Map<Integer, Integer> counts = new HashMap<>();
+        for (int prefix : prefixes) {
+            result += counts.getOrDefault(prefix, 0);
+            counts.compute(prefix + goal, (key, oldValue) -> oldValue == null ? 1 : oldValue + 1);
         }
         return result;
     }
