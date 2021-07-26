342747ebd2c412fd9830f73caf667e631d2e6d40
tau3 <smr.oznob@gmail.com>
2021-07-26 17:19:04 +0400

    solved kth_smallest_element_in_a_bst

---
+++ leetcode/rust/src/kth_smallest_element_in_a_bst.rs
@@ -0,0 +1,41 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+        let mut values = vec![];
+        Solution::in_order_traversal(&root, &mut values);
+        values[(k - 1) as usize]
+    }
+
+    fn in_order_traversal(node: &Option<Rc<RefCell<TreeNode>>>, values: &mut Vec<i32>) {
+        if node.is_none() {
+            return;
+        }
+
+        let node = node.as_ref().unwrap();
+        Solution::in_order_traversal(&node.borrow().left, values);
+        values.push(node.borrow().val);
+        Solution::in_order_traversal(&node.borrow().right, values);
+    }
+}
--- leetcode/rust/src/main.rs
+++ leetcode/rust/src/main.rs
@@ -1,4 +1,5 @@
 mod check_if_two_string_arrays_are_equivalent;
 mod smallest_range_2;
+mod kth_smallest_element_in_a_bst;
 
 fn main() {}
