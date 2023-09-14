01e83ec93c3b32c343383f7c74f0d8d5203f4268
tau3 <smr.oznob@gmail.com>
2023-09-14 18:40:49 +0400

    speed up solution

--- leetcode/rust/src/find_the_pivot_integer.rs
+++ leetcode/rust/src/find_the_pivot_integer.rs
@@ -2,34 +2,30 @@ pub struct Solution {}
 
 impl Solution {
     pub fn pivot_integer(n: i32) -> i32 {
+        let mut total = 0;
         for x in 1..=n {
-	    let mut before = 0;
-	    for i in 1..=x {
-		before += i;
-	    }
-	    let mut after = 0;
-	    for i in x..=n {
-		after += i;
-	    }
+            total += x;
+        }
 
-	    if after == before {
-		return x;
-	    }
-	}
-	-1
+        let mut right = 0;
+        for x in (1..=n).rev() {
+            right += x;
+            if total - right + x == right {
+                return x;
+            }
+        }
+        -1
     }
 }
-
 
 #[cfg(test)]
 mod p2485_tests {
     use super::*;
 
     #[test]
-    fn test(){
-	assert_eq!(Solution::pivot_integer(8), 6);
-	assert_eq!(Solution::pivot_integer(1), 1);
-	assert_eq!(Solution::pivot_integer(4), -1);
+    fn test() {
+        assert_eq!(Solution::pivot_integer(8), 6);
+        assert_eq!(Solution::pivot_integer(1), 1);
+        assert_eq!(Solution::pivot_integer(4), -1);
     }
 }
-
