28cab53bf753e3f546d711ab7f26f9f289035707
tau3 <smr.oznob@gmail.com>
2023-09-15 18:59:38 +0400

    fix bug, add more tests

--- leetcode/rust/src/string_odd_counts.rs
+++ leetcode/rust/src/string_odd_counts.rs
@@ -2,17 +2,20 @@ pub struct Solution {}
 
 impl Solution {
     pub fn generate_the_string(n: i32) -> String {
+        if n == 1 {
+            return 'a'.into();
+        }
         if n % 2 != 0 {
             let mut result = Solution::generate_the_string(n - 1);
             result.push('c');
-	    return result;
+            result
         } else {
             let mut result = String::with_capacity(n as usize + 1);
             for _ in 0..n - 1 {
                 result.push('a');
             }
             result.push('b');
-            return result;
+            result
         }
     }
 }
@@ -25,9 +28,17 @@ mod test {
 
     #[test]
     fn test() {
-        assert!(check(&Solution::generate_the_string(19)));
-        assert!(check(&Solution::generate_the_string(500)));
-        assert!(check(&Solution::generate_the_string(1)));
+        let odd = Solution::generate_the_string(19);
+        assert!(check(&odd));
+        assert_eq!(odd.len(), 19);
+
+        let long_even = Solution::generate_the_string(500);
+        assert!(check(&long_even));
+        assert_eq!(long_even.len(), 500);
+
+        let one = Solution::generate_the_string(1);
+        assert!(check(&one));
+        assert_eq!(one.len(), 1);
     }
 
     fn check(input: &String) -> bool {
@@ -38,7 +49,6 @@ mod test {
                 .and_modify(|counter| *counter += 1)
                 .or_insert(1);
         }
-	println!("{}", counter);
         for x in counter.values() {
             if x % 2 == 0 {
                 return false;
