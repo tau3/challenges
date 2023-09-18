20ac64ffae70ce4be59b7beab02f9f104912e25e
tau3 <smr.oznob@gmail.com>
2023-09-18 16:58:50 +0400

    refactor code, add broken test

--- leetcode/rust/src/longest_ideal_subsequence.rs
+++ leetcode/rust/src/longest_ideal_subsequence.rs
@@ -1,6 +1,7 @@
 pub struct Solution {}
 
 impl Solution {
+    #[allow(clippy::needless_range_loop)]
     pub fn longest_ideal_string(s: String, k: i32) -> i32 {
         if s.len() == 1 {
             return 1;
@@ -16,17 +17,23 @@ impl Solution {
                     memo[i][j] = 1;
                     continue;
                 }
-                memo[i][j] = if (letters[j] as i32 - letters[j - 1] as i32).abs() <= k {
-                    memo[i][j - 1] + 1
+		
+                if Self::diff(letters[j], letters[j - 1]) <= k {
+                    memo[i][j] = memo[i][j - 1] + 1;
                 } else {
-                    memo[i][j - 1]
-                };
+                    memo[i][j] = memo[i][j - 1];
+                }
+
                 if memo[i][j] > result {
                     result = memo[i][j];
                 }
             }
         }
         result as i32
+    }
+
+    fn diff(l: char, r: char) -> i32 {
+        (l as i32 - r as i32).abs()
     }
 }
 
@@ -39,5 +46,10 @@ mod test {
         assert_eq!(4, Solution::longest_ideal_string("acfgbd".into(), 2));
         assert_eq!(4, Solution::longest_ideal_string("abcd".into(), 3));
         assert_eq!(1, Solution::longest_ideal_string("a".into(), 3));
+    }
+
+    #[test]
+    fn test_broken() {
+        assert_eq!(5, Solution::longest_ideal_string("jxhwaysa".into(), 14));
     }
 }
