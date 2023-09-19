554600b17d9c54574880965ab215905652aee7f3
tau3 <smr.oznob@gmail.com>
2023-09-19 16:06:54 +0400

    give better names

--- leetcode/rust/src/movie_rental_system.rs
+++ leetcode/rust/src/movie_rental_system.rs
@@ -11,38 +11,41 @@ impl MovieRentingSystem {
     pub fn new(_: i32, entries: Vec<Vec<i32>>) -> Self {
         let mut shops = HashMap::new();
         for entry in entries {
-            let shop_id = entry[0];
-            let movie_id = entry[1];
+            let shop = entry[0];
+            let movie = entry[1];
             let price = entry[2];
             shops
-                .entry(shop_id)
+                .entry(shop)
                 .or_insert_with(HashMap::new)
-                .insert(movie_id, (price, true));
+                .insert(movie, (price, true));
         }
         Self { shops }
     }
 
     pub fn search(&self, movie: i32) -> Vec<i32> {
         let mut result = vec![];
-        for (shop_id, shop_data) in self.shops.iter() {
+        for (shop, shop_data) in self.shops.iter() {
             if let Some((price, is_present)) = shop_data.get(&movie) {
                 if *is_present {
-                    result.push((shop_id, price));
+                    result.push((shop, price));
                 }
             }
         }
-        result.sort_by(|a, b| match a.1.cmp(b.1) {
-            Less => Less,
-            Greater => Greater,
-            Equal => a.0.cmp(b.0),
+        result.sort_by(|(shop_left, price_left), (shop_right, price_right)| {
+            match price_left.cmp(price_right) {
+                Less => Less,
+                Greater => Greater,
+                Equal => shop_left.cmp(shop_right),
+            }
         });
-        result.iter().take(5).map(|(shop_id, _)| **shop_id).collect()
+        result.iter().take(5).map(|(shop, _)| **shop).collect()
     }
 
     pub fn rent(&mut self, shop: i32, movie: i32) {
         let shop_data = &mut self.shops.get_mut(&shop).unwrap();
-        if let Some(x) = shop_data.get_mut(&movie) {
-            *x = (x.0, false);
+        if let Some(entry) = shop_data.get_mut(&movie) {
+            let price = entry.0;
+            *entry = (price, false);
         } else {
             panic!("shop {} doesn't have movie {}", shop, movie);
         }
@@ -50,8 +53,9 @@ impl MovieRentingSystem {
 
     pub fn drop(&mut self, shop: i32, movie: i32) {
         let shop_data = &mut self.shops.get_mut(&shop).unwrap();
-        if let Some(x) = shop_data.get_mut(&movie) {
-            *x = (x.0, true);
+        if let Some(entry) = shop_data.get_mut(&movie) {
+            let price = entry.0;
+            *entry = (price, true);
         } else {
             panic!("shop {} doesn't have movie {}", shop, movie);
         }
@@ -59,28 +63,30 @@ impl MovieRentingSystem {
 
     pub fn report(&self) -> Vec<Vec<i32>> {
         let mut result = vec![];
-        for (shop_id, movie_to_stats) in self.shops.iter() {
-            for (movie_id, stats) in movie_to_stats {
-                if !stats.1 {
+        for (shop, movie_to_stats) in self.shops.iter() {
+            for (movie, (price, is_present)) in movie_to_stats {
+                if !is_present {
                     continue;
                 }
-                result.push((shop_id, movie_id, stats.0));
+                result.push((shop, movie, price));
             }
         }
 
-        result.sort_by(|a, b| match a.2.cmp(&b.2) {
-            Less => Less,
-            Greater => Greater,
-            Equal => match a.0.cmp(b.0) {
+        result.sort_by(|(shop1, movie1, price1), (shop2, movie2, price2)| {
+            match price1.cmp(&price2) {
                 Less => Less,
                 Greater => Greater,
-                Equal => a.1.cmp(b.1),
-            },
+                Equal => match shop1.cmp(shop2) {
+                    Less => Less,
+                    Greater => Greater,
+                    Equal => movie1.cmp(movie2),
+                },
+            }
         });
         result
             .iter()
             .take(5)
-            .map(|(shop_id, movie_id, _)| vec![**shop_id, **movie_id])
+            .map(|(shop, movie, _)| vec![**shop, **movie])
             .collect()
     }
 }
