49ed7b433c0c8d0654f9581def58e747a8472e3f
tau3 <smr.oznob@gmail.com>
2023-09-19 17:16:28 +0400

    use ordered sets

--- leetcode/rust/src/movie_rental_system.rs
+++ leetcode/rust/src/movie_rental_system.rs
@@ -1,78 +1,58 @@
-use std::collections::HashMap;
+use std::collections::{BTreeSet, HashMap};
 
 pub struct MovieRentingSystem {
-    shops: HashMap<i32, HashMap<i32, (i32, bool)>>,
+    absent: BTreeSet<(i32, i32, i32)>,           // price, shop, movie
+    present: HashMap<i32, BTreeSet<(i32, i32)>>, // movie -> (price, shop)
+    prices: HashMap<(i32, i32), i32>,            // (shop, movie) -> price
 }
 
 impl MovieRentingSystem {
     pub fn new(_: i32, entries: Vec<Vec<i32>>) -> Self {
-        let mut shops = HashMap::new();
+        let mut present = HashMap::new();
+        let mut prices = HashMap::new();
         for entry in entries {
             let shop = entry[0];
             let movie = entry[1];
             let price = entry[2];
-            shops
-                .entry(shop)
-                .or_insert_with(HashMap::new)
-                .insert(movie, (price, true));
+            present
+                .entry(movie)
+                .or_insert_with(BTreeSet::new)
+                .insert((price, shop));
+            prices.insert((shop, movie), price);
         }
-        Self { shops }
+        Self {
+            absent: BTreeSet::new(),
+            present,
+            prices,
+        }
     }
 
     pub fn search(&self, movie: i32) -> Vec<i32> {
-        let mut result = vec![];
-        for (shop, shop_data) in self.shops.iter() {
-            if let Some((price, is_present)) = shop_data.get(&movie) {
-                if *is_present {
-                    result.push((shop, price));
-                }
-            }
-        }
-        result
-            .sort_by(|(shop1, price1), (shop2, price2)| price1.cmp(price2).then(shop1.cmp(shop2)));
-        result.iter().take(5).map(|(shop, _)| **shop).collect()
+        let data = self.present.get(&movie).unwrap();
+        data.iter().take(5).map(|(_, shop)| *shop).collect()
     }
 
     pub fn rent(&mut self, shop: i32, movie: i32) {
-        let shop_data = &mut self.shops.get_mut(&shop).unwrap();
-        if let Some(entry) = shop_data.get_mut(&movie) {
-            let price = entry.0;
-            *entry = (price, false);
-        } else {
-            panic!("shop {} doesn't have movie {}", shop, movie);
-        }
+        let price = *self.prices.get(&(shop, movie)).unwrap();
+        self.absent.insert((price, shop, movie));
+
+        let data = self.present.get_mut(&movie).unwrap();
+        data.remove(&(price, shop));
     }
 
     pub fn drop(&mut self, shop: i32, movie: i32) {
-        let shop_data = &mut self.shops.get_mut(&shop).unwrap();
-        if let Some(entry) = shop_data.get_mut(&movie) {
-            let price = entry.0;
-            *entry = (price, true);
-        } else {
-            panic!("shop {} doesn't have movie {}", shop, movie);
-        }
+        let price = *self.prices.get(&(shop, movie)).unwrap();
+        self.absent.remove(&(price, shop, movie));
+
+        let data = self.present.get_mut(&movie).unwrap();
+        data.insert((price, shop));
     }
 
     pub fn report(&self) -> Vec<Vec<i32>> {
-        let mut result = vec![];
-        for (shop, movie_to_stats) in self.shops.iter() {
-            for (movie, (price, is_present)) in movie_to_stats {
-                if *is_present {
-                    continue;
-                }
-                result.push((shop, movie, price));
-            }
-        }
-
-        result.sort_by(|(shop1, movie1, price1), (shop2, movie2, price2)| {
-            price1
-                .cmp(price2)
-                .then(shop1.cmp(shop2).then(movie1.cmp(movie2)))
-        });
-        result
+        self.absent
             .iter()
             .take(5)
-            .map(|(shop, movie, _)| vec![**shop, **movie])
+            .map(|(_, shop, movie)| vec![*shop, *movie])
             .collect()
     }
 }
