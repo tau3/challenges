7417548fc62027c4a3bf9e558980a6cffab38e43
tau3 <smr.oznob@gmail.com>
2023-09-27 00:24:13 +0400

    make it work

--- leetcode/rust/src/largest_palindromic_number.rs
+++ leetcode/rust/src/largest_palindromic_number.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, VecDeque};
 
 pub struct Solution {}
 
@@ -10,11 +10,11 @@ impl Solution {
             *value += 1;
         }
 
-        let mut right_half = String::from("");
+        let mut right_half = VecDeque::new();
         for (digit, count) in counters.iter_mut() {
             let half = *count / 2;
             for _ in 0..half {
-                right_half.push(*digit);
+                right_half.push_back(*digit);
             }
             *count -= half * 2;
         }
@@ -25,12 +25,20 @@ impl Solution {
             .map(|(digit, _)| digit)
             .max();
 
-        let mut result: String = right_half.chars().rev().collect();
+        let mut result: VecDeque<char> = right_half.iter().copied().rev().collect();
         if let Some(middle) = maybe_middle {
-            result.push(*middle);
+            result.push_back(*middle);
         }
-        result.push_str(&right_half);
-        result
+        result.append(&mut right_half);
+
+        while let Some('0') = result.back() {
+            result.pop_back();
+            result.pop_front();
+        }
+        if result.is_empty() {
+            result.push_back('0');
+        }
+        result.iter().collect()
     }
 }
 
@@ -48,6 +56,22 @@ mod test {
         assert_eq!(
             "9".to_string(),
             Solution::largest_palindromic("00009".into())
+        );
+    }
+
+    #[test]
+    fn test_only_zeros() {
+        assert_eq!(
+            "0".to_string(),
+            Solution::largest_palindromic("00000".into())
+        );
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(
+            "1009001".to_string(),
+            Solution::largest_palindromic("11000094".into())
         );
     }
 }
