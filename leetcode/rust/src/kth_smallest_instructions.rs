5616585e36cd8c6f49f1feaa63fa2ade819b7267
tau3 <smr.oznob@gmail.com>
2023-10-09 17:18:18 +0400

    fighting with borrow checker

--- leetcode/rust/src/kth_smallest_instructions.rs
+++ leetcode/rust/src/kth_smallest_instructions.rs
@@ -1,19 +1,19 @@
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap};
 
 pub struct Solution {}
 
 impl Solution {
     pub fn kth_smallest_path(destination: Vec<i32>, k: i32) -> String {
-        let mut memo = HashMap::new();
-        let mut all_paths = Self::solve(destination[0], destination[1], &mut memo);
+        let memo = RefCell::new(HashMap::new());
+        let mut all_paths = Self::solve(destination[0], destination[1], &memo);
         all_paths.sort();
-        return all_paths[k as usize - 1];
+        all_paths[k as usize - 1].clone()
     }
 
     fn solve(
         row: i32,
         column: i32,
-        mut memo: &mut HashMap<(i32, i32), Vec<String>>,
+        memo: &RefCell<HashMap<(i32, i32), Vec<String>>>,
     ) -> Vec<String> {
         if row < 0 || column < 0 {
             return vec![];
@@ -26,20 +26,27 @@ impl Solution {
             return vec!["V".to_string()];
         }
 
-        let from_top = memo
-            .entry((row, column))
-            .or_insert_with(|| Self::solve(row - 1, column, memo));
-        let from_left = memo
-            .entry((row, column))
-            .or_insert_with(|| Self::solve(row, column - 1, memo));
+        if !memo.borrow().contains_key(&(row - 1, column)) {
+            let paths = Self::solve(row - 1, column, memo);
+            memo.borrow_mut().insert((row - 1, column), paths);
+        }
+        let binding = memo.borrow();
+        let from_left = binding.get(&(row - 1, column)).unwrap();
+
+        if !memo.borrow().contains_key(&(row, column - 1)) {
+            let paths = Self::solve(row, column - 1, memo);
+            memo.borrow_mut().insert((row, column - 1), paths);
+        }
+        let binding = memo.borrow();
+        let from_top = binding.get(&(row, column - 1)).unwrap();
 
         let mut result = Vec::with_capacity(from_top.len() + from_left.len());
-        for path in from_top {
+        for path in from_top.iter() {
             let mut path = path.clone();
             path.push('V');
             result.push(path);
         }
-        for path in from_left {
+        for path in from_left.iter() {
             let mut path = path.clone();
             path.push('H');
             result.push(path);
