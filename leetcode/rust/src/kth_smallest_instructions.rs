07307b474036e91383af28580853a05c4494c5bf
tau3 <smr.oznob@gmail.com>
2023-10-09 17:51:05 +0400

    make wortk

--- leetcode/rust/src/kth_smallest_instructions.rs
+++ leetcode/rust/src/kth_smallest_instructions.rs
@@ -1,19 +1,18 @@
-use std::{cell::RefCell, collections::HashMap};
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 pub struct Solution {}
 
 impl Solution {
     pub fn kth_smallest_path(destination: Vec<i32>, k: i32) -> String {
-        let memo = RefCell::new(HashMap::new());
-        let mut all_paths = Self::solve(destination[0], destination[1], &memo);
+        let memo = Rc::new(RefCell::new(HashMap::new()));
+        let mut all_paths = Self::solve((destination[0], destination[1]), memo);
         all_paths.sort();
         all_paths[k as usize - 1].clone()
     }
 
     fn solve(
-        row: i32,
-        column: i32,
-        memo: &RefCell<HashMap<(i32, i32), Vec<String>>>,
+        (row, column): (i32, i32),
+        memo: Rc<RefCell<HashMap<(i32, i32), Vec<String>>>>,
     ) -> Vec<String> {
         if row < 0 || column < 0 {
             return vec![];
@@ -26,19 +25,22 @@ impl Solution {
             return vec!["V".to_string()];
         }
 
-        if !memo.borrow().contains_key(&(row - 1, column)) {
-            let paths = Self::solve(row - 1, column, memo);
+        let left = (row - 1, column);
+        let need_calc_from_left = !memo.borrow().contains_key(&left);
+        if need_calc_from_left {
+            let paths = Self::solve(left, Rc::clone(&memo));
             memo.borrow_mut().insert((row - 1, column), paths);
         }
-        let binding = memo.borrow();
-        let from_left = binding.get(&(row - 1, column)).unwrap();
-
-        if !memo.borrow().contains_key(&(row, column - 1)) {
-            let paths = Self::solve(row, column - 1, memo);
-            memo.borrow_mut().insert((row, column - 1), paths);
+        let top = (row, column - 1);
+        let need_calc_from_top = !memo.borrow().contains_key(&top);
+        if need_calc_from_top {
+            let paths = Self::solve(top, Rc::clone(&memo));
+            memo.borrow_mut().insert(top, paths);
         }
-        let binding = memo.borrow();
-        let from_top = binding.get(&(row, column - 1)).unwrap();
+
+        let memo = memo.borrow();
+        let from_left = memo.get(&left).unwrap();
+        let from_top = memo.get(&top).unwrap();
 
         let mut result = Vec::with_capacity(from_top.len() + from_left.len());
         for path in from_top.iter() {
