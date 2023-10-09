031ae8e045ff72aff066d4613cc7a9aa684341b4
tau3 <smr.oznob@gmail.com>
2023-10-09 18:19:26 +0400

    fixing errors

--- leetcode/rust/src/kth_smallest_instructions.rs
+++ leetcode/rust/src/kth_smallest_instructions.rs
@@ -2,22 +2,24 @@ use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 pub struct Solution {}
 
+type Memo = Rc<RefCell<HashMap<(i32, i32), Vec<String>>>>;
+
 impl Solution {
     pub fn kth_smallest_path(destination: Vec<i32>, k: i32) -> String {
         let memo = Rc::new(RefCell::new(HashMap::new()));
-        let mut all_paths = Self::solve((destination[0], destination[1]), memo);
+        let mut all_paths = Self::solve((destination[0], destination[1]), Rc::clone(&memo));
         all_paths.sort();
         all_paths[k as usize - 1].clone()
     }
 
-    fn solve(
-        (row, column): (i32, i32),
-        memo: Rc<RefCell<HashMap<(i32, i32), Vec<String>>>>,
-    ) -> Vec<String> {
+    fn solve((row, column): (i32, i32), memo: Memo) -> Vec<String> {
         if row < 0 || column < 0 {
             return vec![];
         }
 
+        if row == 0 && column == 0 {
+            return vec![];
+        }
         if row == 0 && column == 1 {
             return vec!["H".to_string()];
         }
@@ -25,32 +27,30 @@ impl Solution {
             return vec!["V".to_string()];
         }
 
-        let left = (row - 1, column);
-        let need_calc_from_left = !memo.borrow().contains_key(&left);
-        if need_calc_from_left {
-            let paths = Self::solve(left, Rc::clone(&memo));
-            memo.borrow_mut().insert((row - 1, column), paths);
-        }
-        let top = (row, column - 1);
-        let need_calc_from_top = !memo.borrow().contains_key(&top);
-        if need_calc_from_top {
+        let top = (row - 1, column);
+        if !memo.borrow().contains_key(&top) {
             let paths = Self::solve(top, Rc::clone(&memo));
             memo.borrow_mut().insert(top, paths);
         }
+        let left = (row, column - 1);
+        if !memo.borrow().contains_key(&left) {
+            let paths = Self::solve(left, Rc::clone(&memo));
+            memo.borrow_mut().insert(left, paths);
+        }
 
         let memo = memo.borrow();
-        let from_left = memo.get(&left).unwrap();
         let from_top = memo.get(&top).unwrap();
+        let from_left = memo.get(&left).unwrap();
 
-        let mut result = Vec::with_capacity(from_top.len() + from_left.len());
-        for path in from_top.iter() {
-            let mut path = path.clone();
-            path.push('V');
-            result.push(path);
-        }
+        let mut result = Vec::with_capacity(from_left.len() + from_top.len());
         for path in from_left.iter() {
             let mut path = path.clone();
             path.push('H');
+            result.push(path);
+        }
+        for path in from_top.iter() {
+            let mut path = path.clone();
+            path.push('V');
             result.push(path);
         }
 
@@ -65,5 +65,12 @@ mod test {
     #[test]
     fn test() {
         assert_eq!("HHHVV", Solution::kth_smallest_path(vec![2, 3], 1));
+        assert_eq!("HHVHV", Solution::kth_smallest_path(vec![2, 3], 2));
+        assert_eq!("HHVVH", Solution::kth_smallest_path(vec![2, 3], 3));
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!("HV", Solution::kth_smallest_path(vec![1, 1], 1));
     }
 }
