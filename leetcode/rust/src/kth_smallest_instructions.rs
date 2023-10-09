6a3f5e49cc20966c01cf018122a915b6c477d987
tau3 <smr.oznob@gmail.com>
2023-10-09 17:00:56 +0400

    use memo

--- leetcode/rust/src/kth_smallest_instructions.rs
+++ leetcode/rust/src/kth_smallest_instructions.rs
@@ -1,14 +1,20 @@
+use std::collections::HashMap;
+
 pub struct Solution {}
 
 impl Solution {
     pub fn kth_smallest_path(destination: Vec<i32>, k: i32) -> String {
-        let mut all_paths = Self::solve(destination[0], destination[1]);
+        let mut memo = HashMap::new();
+        let mut all_paths = Self::solve(destination[0], destination[1], &mut memo);
         all_paths.sort();
         return all_paths[k as usize - 1];
     }
 
-    // TODO memo
-    fn solve(row: i32, column: i32) -> Vec<String> {
+    fn solve(
+        row: i32,
+        column: i32,
+        mut memo: &mut HashMap<(i32, i32), Vec<String>>,
+    ) -> Vec<String> {
         if row < 0 || column < 0 {
             return vec![];
         }
@@ -20,15 +26,24 @@ impl Solution {
             return vec!["V".to_string()];
         }
 
-        let from_top = Self::solve(row - 1, column);
-        let from_left = Self::solve(row, column - 1);
+        let from_top = memo
+            .entry((row, column))
+            .or_insert_with(|| Self::solve(row - 1, column, memo));
+        let from_left = memo
+            .entry((row, column))
+            .or_insert_with(|| Self::solve(row, column - 1, memo));
 
-        let mut result: Vec<String> = from_top.iter().map(|x| x.push('V')).collect();
-
-        from_left
-            .iter()
-            .map(|x| x.push('H'))
-            .for_each(|x| result.push_back(x));
+        let mut result = Vec::with_capacity(from_top.len() + from_left.len());
+        for path in from_top {
+            let mut path = path.clone();
+            path.push('V');
+            result.push(path);
+        }
+        for path in from_left {
+            let mut path = path.clone();
+            path.push('H');
+            result.push(path);
+        }
 
         result
     }
