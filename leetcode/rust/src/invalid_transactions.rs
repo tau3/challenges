39025fd74f2980d9327fc989955e27cd307cc2ea
tau3 <smr.oznob@gmail.com>
2023-10-13 18:03:05 +0400

    improve tests

--- leetcode/rust/src/invalid_transactions.rs
+++ leetcode/rust/src/invalid_transactions.rs
@@ -14,6 +14,7 @@ impl Solution {
             if current_amount > 1000 {
                 result.insert(current.clone());
             }
+            let current_city = current_tokens.next().unwrap();
             for j in i + 1..transactions.len() {
                 let other = &transactions[j];
                 let mut other_tokens = other.split(',');
@@ -21,7 +22,6 @@ impl Solution {
                 if other_name == current_name {
                     let other_time: i16 = other_tokens.next().unwrap().parse().unwrap();
                     if (other_time - current_time).abs() <= 60 {
-                        let current_city = current_tokens.next().unwrap();
                         let _ = other_tokens.next();
                         let other_city = other_tokens.next().unwrap();
                         if other_city != current_city {
@@ -39,51 +39,92 @@ impl Solution {
 
 #[cfg(test)]
 mod test {
-
     use super::*;
 
     #[test]
     fn test() {
-        assert_eq!(
-            vec!["alice,50,1200,mtv"],
+        assert_eq_any_order(
+            vec!["alice,50,1200,mtv".to_string()],
             Solution::invalid_transactions(vec![
                 "alice,20,800,mtv".to_string(),
-                "alice,50,1200,mtv".to_string()
-            ])
+                "alice,50,1200,mtv".to_string(),
+            ]),
         );
     }
 
     #[test]
     fn test2() {
-        assert_eq!(
-            vec!["alice,20,800,mtv", "alice,50,100,beijing"],
+        assert_eq_any_order(
+            vec![
+                "alice,20,800,mtv".to_string(),
+                "alice,50,100,beijing".to_string(),
+            ],
             Solution::invalid_transactions(vec![
                 "alice,50,100,beijing".to_string(),
-                "alice,20,800,mtv".to_string()
-            ])
+                "alice,20,800,mtv".to_string(),
+            ]),
         );
     }
 
     #[test]
     fn test3() {
-        assert_eq!(
-            vec!["alice,50,1200,mtv"],
+        assert_eq_any_order(
+            vec!["alice,50,1200,mtv".to_string()],
             Solution::invalid_transactions(vec![
                 "alice,20,800,mtv".to_string(),
-                "alice,50,1200,mtv".to_string()
-            ])
+                "alice,50,1200,mtv".to_string(),
+            ]),
         );
     }
 
     #[test]
     fn test4() {
-        assert_eq!(
-            vec!["alice,50,1200,mtv"],
+        assert_eq_any_order(
+            vec![
+                "alice,20,800,mtv".to_string(),
+                "alice,50,100,mtv".to_string(),
+                "alice,51,100,frankfurt".to_string(),
+            ],
             Solution::invalid_transactions(vec![
                 "alice,20,800,mtv".to_string(),
                 "alice,50,100,mtv".to_string(),
-                "alice,51,100,frankfurt".to_string()
-            ])
+                "alice,51,100,frankfurt".to_string(),
+            ]),
         );
+    }
+
+    #[test]
+    fn test5() {
+        assert_eq_any_order(
+            vec![
+                "alice,20,1220,mtv".to_string(),
+                "alice,20,1220,mtv".to_string(),
+            ],
+            Solution::invalid_transactions(vec![
+                "alice,20,1220,mtv".to_string(),
+                "alice,20,1220,mtv".to_string(),
+            ]),
+        );
+    }
+
+    fn assert_eq_any_order(mut left: Vec<String>, mut right: Vec<String>) {
+        if left.len() != right.len() {
+            panic!("different len: {:?} and {:?}", left, right);
+        }
+
+        left.sort();
+        right.sort();
+
+        let mut result = true;
+        for i in 0..left.len() {
+            if left[i] != right[i] {
+                result = false;
+                break;
+            }
+        }
+
+        if !result {
+            panic!("{:?} != {:?}", left, right);
+        }
     }
 }
